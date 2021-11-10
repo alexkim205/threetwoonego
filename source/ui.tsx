13ac7695f720bfd92812f9b4381252f5c13993d4
Alex Kim <alexgkim205@gmail.com>
2021-11-09 23:41:41 -0800

    add react reactdom babel lint-staged

---
+++ source/ui.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function initUi() {
+  console.log('CONTENT SCRIPT', Button)
+}
+
+function Button(): JSX.Element {
+  return (
+    <button>
+			hello asdfasd
+    </button>
+  )
+}
+
+export default initUi
